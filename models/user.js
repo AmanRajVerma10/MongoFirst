a9215f6c60022daae12a1a086667ab5d19fbb356
AmanRajVerma10 <verma.aman464@gmail.com>
2024-04-12 14:55:10 +0530

    working with user model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,14 @@
 const mongodb = require("mongodb");
+const { get } = require("../routes/admin");
 
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   save() {
     let db = getDb();
@@ -19,7 +22,15 @@ class User {
         console.log(e);
       });
   }
-  findUserById(userId) {
+  addToCart(product) {
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
+  }
+  static findById(userId) {
     return getDb()
       .collection("users")
       .findOne({ _id: new mongodb.ObjectId(userId) })
