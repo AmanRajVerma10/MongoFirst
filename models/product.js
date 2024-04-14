941f014926bd6a1f84ec8a4df0179893982a2ca6
AmanRajVerma10 <verma.aman464@gmail.com>
2024-04-15 01:12:39 +0530

    CRUD using mongoose

--- models/product.js
+++ models/product.js
@@ -1,77 +1,94 @@
-const mongodb = require("mongodb");
+const mongoose=require('mongoose')
 
-const getDb = require("../util/database").getDb;
+const Schema= mongoose.Schema;
 
-class Product {
-  constructor(title, price, description, imageUrl, id,userId) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this._id = id? new mongodb.ObjectId(id):null;
-    this.userId=userId;
-  }
-  save() {
-    const db = getDb();
-    let dbOp;
-    if (this._id) {
-      dbOp = db
-        .collection("products")
-        .updateOne({ _id: this._id}, { $set: this });
-    } else {
-      dbOp = db.collection("products").insertOne(this);
-    }
-    return dbOp
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-  static fetchAll() {
-    const db = getDb();
-    return db
-      .collection("products")
-      .find()
-      .toArray()
-      .then((products) => {
-        console.log(products);
-        return products;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
+const productSchema= new Schema({
+  title:{type: String,
+  required:true},
+  price:{type:Number,
+  required:true},
+  description:{type: String,
+  required: true},
+  imageUrl:{type:String,
+  required:true}
+})
 
-  static findById(prodId) {
-    const db = getDb();
-    return db
-      .collection("products")
-      .find({ _id: new mongodb.ObjectId(prodId) })
-      .next()
-      .then((product) => {
-        console.log(product);
-        return product;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
+module.exports=mongoose.model('Product',productSchema);
 
-  static deleteById(prodId) {
-    const db = getDb();
-    return db
-      .collection("products")
-      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
-      .then((result) => {
-        console.log(result);
-        return result;
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-}
+// const mongodb = require("mongodb");
 
-module.exports = Product;
+// const getDb = require("../util/database").getDb;
+
+// class Product {
+//   constructor(title, price, description, imageUrl, id,userId) {
+//     this.title = title;
+//     this.price = price;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this._id = id? new mongodb.ObjectId(id):null;
+//     this.userId=userId;
+//   }
+//   save() {
+//     const db = getDb();
+//     let dbOp;
+//     if (this._id) {
+//       dbOp = db
+//         .collection("products")
+//         .updateOne({ _id: this._id}, { $set: this });
+//     } else {
+//       dbOp = db.collection("products").insertOne(this);
+//     }
+//     return dbOp
+//       .then((result) => {
+//         console.log(result);
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+//   static fetchAll() {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .find()
+//       .toArray()
+//       .then((products) => {
+//         console.log(products);
+//         return products;
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+
+//   static findById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .find({ _id: new mongodb.ObjectId(prodId) })
+//       .next()
+//       .then((product) => {
+//         console.log(product);
+//         return product;
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+
+//   static deleteById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+//       .then((result) => {
+//         console.log(result);
+//         return result;
+//       })
+//       .catch((e) => {
+//         console.log(e);
+//       });
+//   }
+// }
+
+// module.exports = Product;
